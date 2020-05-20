41bbb71c41e539842b724fc9b82561b987f61abd
POMAH <POMAH>
2020-05-20 23:18:12 +0300

    определение типов клетки карты

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <Windows.h>
 
+enum tupe_cell
+{
+		grass = 'g',// свободная клетка
+		bush = 'b',// клетка, удержавающая врагов и камни
+		stone = 's',// камень
+		wall = 'w',// стена
+		// враги и персонаж накладываются на карту
+};
+
 void main()
 {
 	printf("Hello from POMAH\n");
