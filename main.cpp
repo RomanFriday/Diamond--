9460383fead8edd45ea8ade57d17415a5670d159
POMAH <POMAH>
2020-05-21 12:14:07 +0300

    рисование игрока на отображаемой части карты

--- main.cpp
+++ main.cpp
@@ -20,25 +20,66 @@ enum tupe_cell
 		// враги и персонаж накладываются на карту
 };
 
+// структура игрока (может быть дополнена)
+typedef struct _player
+{
+	COORD pos; // позиция на карте
+	LPCSTR ch; // символ игрока
+	WORD *color; // цвет фона и цвет игрока
+} player;
+
+
 HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 int b=SetConsoleActiveScreenBuffer(hConsole);
 
+// нарисовать игрока на отображаемой части карты
+void print_player_on_screen(COORD screen_pos, player pl)
+{
+	COORD now = {pl.pos.X-screen_pos.X, pl.pos.Y-screen_pos.Y}; // координаты игрока на экране
+	DWORD dw = 0; // число записей на экран
+	if( now.X >= 0 && now.X < MAX_MAP_SCREEN_X)
+		if(now.Y >= 0 && now.Y < MAX_MAP_SCREEN_Y)
+		{
+			WriteConsoleOutputCharacter(hConsole, // дескриптор буфера экрана
+				pl.ch, // строка символов
+				1, // длина строки
+				now, // координаты начала строки на консоли
+				&dw); // количество записей
+			WriteConsoleOutputAttribute(
+				hConsole, // дескриптор экранного буфера
+				pl.color, // строка цветов
+				1, // длина строки
+				now, // координаты начала строки на консоли
+				&dw);// количество записей
+		}
+	return;
+}
+
 // рисование части карты.
 // На вход подаются координаты начала отображения карты, карта(символы+цвета), координаты игрока, список координат врагов
-int print_map(char *map, COORD pos, unsigned short *map_colors, COORD player, COORD *enemies)
+void print_map(char *map,  unsigned short *map_colors, COORD screen_pos, player pl, COORD *enemies)
 {
 	DWORD dw=0; // число записей на экран
-	for(COORD now={0,0}; now.Y < MAX_MAP_SCREEN_Y; now.Y++)
+	COORD now={0,0};
+	// прорисовка карты без игрока и врагов
+	for(; now.Y < MAX_MAP_SCREEN_Y; now.Y++)
 	{
 		WriteConsoleOutputCharacter(hConsole, // дескриптор буфера экрана
-			map + (pos.Y + now.Y)*MAX_MAP_SCREEN_X, // строка 
+			map + (screen_pos.Y + now.Y)*MAX_MAP_SCREEN_X, // строка символов
 			MAX_MAP_SCREEN_X, // длина строки
 			now, // координаты начала строки на консоли
 			&dw); // количество записей
+		WriteConsoleOutputAttribute(
+			hConsole, // дескриптор экранного буфера
+			map_colors+(screen_pos.Y + now.Y)*MAX_MAP_SCREEN_X, // строка цветов
+			MAX_MAP_SCREEN_X, // длина строки
+			now, // координаты начала строки на консоли
+			&dw);// количество записей
 	}
-	/* рисование играка */
+	// рисование игрока, если он находится на отображаемой части экрана
+	print_player_on_screen(screen_pos, pl);
 	/* рисование врагов */
-	return 0;
+	return;
 }
 
 void main()
