db0eda97b6dddef94936d3c758718d5ecc79313c
Eugene <Eugene@mail.ru>
2020-06-16 03:53:04 +0300

    Функция проверки возможности хода

--- main.cpp
+++ main.cpp
@@ -900,10 +900,6 @@ void screen_position(COORD *screen_pos, s_player *player, s_map *map)
 	*screen_pos = pos;
 }
 
-int can_i_move(s_map *map, direction dir, int X, int Y)
-{
-	return 1;
-}
 
 // является ли клетка камнем
 int is_stone(s_map *map, int X, int Y)
@@ -912,6 +908,80 @@ int is_stone(s_map *map, int X, int Y)
 		return 0;
 	if(map->matr[Y][X].ch == type_p_stone || map->matr[Y][X].ch == type_stone)
 		return 1;
+	return 0;
+}
+int can_i_move_right(direction dir, s_map* map, int X, int Y)
+{
+	if (!is_on_map(map, X + 1, Y))
+		return 0;
+	switch (map->matr[Y][X + 1].ch)
+	{
+	case type_bush: case type_exit: case type_grass: case type_diamond:
+	case type_p_bush: case type_p_exit: case type_p_grass: case type_p_diamond:
+		return 1;
+	case type_stone: case type_p_stone:
+		if (is_grass(map, X + 1, Y))
+			return 1;
+	default: 0;
+	}
+	return 0;
+}
+int can_i_move_left(direction dir, s_map* map, int X, int Y)
+{
+	if (!is_on_map(map, X - 1, Y))
+		return 0;
+	switch (map->matr[Y][X - 1].ch)
+	{
+	case type_bush: case type_exit: case type_grass: case type_diamond:
+	case type_p_bush: case type_p_exit: case type_p_grass: case type_p_diamond:
+		return 1;
+	case type_stone: case type_p_stone:
+		if (is_grass(map, X - 1, Y))
+			return 1;
+	default: 0;
+	}
+	return 0;
+}
+int can_i_move_up(direction dir, s_map* map, int X, int Y)
+{
+	if (!is_on_map(map, X, Y - 1))
+		return 0;
+	switch (map->matr[Y - 1][X].ch)
+	{
+	case type_bush: case type_exit: case type_grass: case type_diamond:
+	case type_p_bush: case type_p_exit: case type_p_grass: case type_p_diamond:
+		return 1;
+	default: 0;
+	}
+	return 0;
+}
+int can_i_move_down(direction dir, s_map* map, int X, int Y)
+{
+	if (!is_on_map(map, X, Y + 1))
+		return 0;
+	switch (map->matr[Y + 1][X].ch)
+	{
+	case type_bush: case type_exit: case type_grass: case type_diamond:
+	case type_p_bush: case type_p_exit: case type_p_grass: case type_p_diamond:
+		return 1;
+	default: 0;
+	}
+	return 0;
+}
+
+int can_i_move(direction dir, s_map* map, int X, int Y)
+{
+	switch (dir)
+	{
+	case right:
+		return can_i_move_right(dir, map, X, Y);
+	case left:
+		return can_i_move_right(dir, map, X, Y);
+	case up:
+		return can_i_move_up(dir, map, X, Y);
+	case down:
+		return can_i_move_down(dir, map, X, Y);
+	}
 	return 0;
 }
 
@@ -923,7 +993,7 @@ int push_stone(s_map *map, direction dir, s_player *player, s_q_stone *q_stone)
 	case left:
 		if(!is_stone(map, player->pos.X-1, player->pos.Y)) // это не камень - выходим
 			return 0;
-		if(!can_i_move(map, dir, player->pos.X, player->pos.Y)) // камень нельзя передвинуть - выходим
+		if(!can_i_move( dir, map, player->pos.X, player->pos.Y)) // камень нельзя передвинуть - выходим
 			return 0;
 		stone = stone_in_q(q_stone, player->pos.X-1, player->pos.Y);
 		map->matr[player->pos.Y][player->pos.X-1].ch = type_p_grass;
@@ -934,7 +1004,7 @@ int push_stone(s_map *map, direction dir, s_player *player, s_q_stone *q_stone)
 	case right:
 		if(!is_stone(map, player->pos.X+1, player->pos.Y)) // это не камень - выходим
 			return 0;
-		if(!can_i_move(map, dir, player->pos.X, player->pos.Y)) // камень нельзя передвинуть - выходим
+		if(!can_i_move( dir, map, player->pos.X, player->pos.Y)) // камень нельзя передвинуть - выходим
 			return 0;
 		stone = stone_in_q(q_stone, player->pos.X+1, player->pos.Y);
 		map->matr[player->pos.Y][player->pos.X+1].ch = type_p_grass;
@@ -975,7 +1045,7 @@ int main()
 				switch(c)
 				{
 				case 'w':
-					if(player.pos.Y>0/*&&map.matr[player.pos.Y-1][player.pos.X].ch+256!=type_p_wall&&map.matr[player.pos.Y-1][player.pos.X].ch!=type_p_stone*/)
+					if(can_i_move(up,&map, player.pos.X, player.pos.Y))
 					{
 						map.matr[player.pos.Y][player.pos.X].pl=NULL;
 						player.pos.Y--;
@@ -983,7 +1053,7 @@ int main()
 					}
 					break;
 				case 's':
-					if(player.pos.Y<map.size.Y-1/*&&map.matr[player.pos.Y+1][player.pos.X].ch+256!=type_p_wall&&map.matr[player.pos.Y+1][player.pos.X].ch!=type_p_stone*/)
+					if(can_i_move(down, &map, player.pos.X, player.pos.Y))
 					{
 						map.matr[player.pos.Y][player.pos.X].pl=NULL;
 						player.pos.Y++;
@@ -991,7 +1061,7 @@ int main()
 					}
 					break;
 				case 'a':
-					if(player.pos.X>0/*&&map.matr[player.pos.Y][player.pos.X-1].ch+256!=type_p_wall&&map.matr[player.pos.Y][player.pos.X-1].ch!=type_p_stone*/)
+					if(can_i_move(left, &map, player.pos.X, player.pos.Y))
 					{
 						push_stone(&map, left, &player, &q_stone);
 						map.matr[player.pos.Y][player.pos.X].pl=NULL;
@@ -1000,7 +1070,7 @@ int main()
 					}
 					break;
 				case 'd':
-					if(player.pos.X<map.size.X-1/*&&map.matr[player.pos.Y][player.pos.X+1].ch+256!=type_p_wall&&map.matr[player.pos.Y][player.pos.X+1].ch!=type_p_stone*/)
+					if(can_i_move(right, &map, player.pos.X, player.pos.Y))
 					{
 						push_stone(&map, right, &player, &q_stone);
 						map.matr[player.pos.Y][player.pos.X].pl=NULL;
